d8ea142f9269748dfb5102d7989c4a208ccb37e1
saltfish <cody741963@hotmail.com>
2025-01-09 13:47:07 -0500

    chore: mysql tune

--- master/repositories/mysql/mysql.go
+++ master/repositories/mysql/mysql.go
@@ -5,18 +5,25 @@ package mysql
 import (
 	"GalaxyEmpireWeb/config"
 	"sync"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-var once sync.Once
-var globalDB *gorm.DB
-var err error
+var (
+	once     sync.Once
+	globalDB *gorm.DB
+	err      error
+)
 
 func ConnectDatabase() {
 	dsn := config.GetDSN("config/yaml/mysql.yaml")
 	globalDB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, _ := globalDB.DB()
+	db.SetMaxIdleConns(5)
+	db.SetMaxOpenConns(50)
+	db.SetConnMaxLifetime(time.Hour)
 	if err != nil {
 		panic("failed to connect database")
 	}
